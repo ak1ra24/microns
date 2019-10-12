9d37ba2efaa8fba542538c961094026e475881f8
ak1ra24 <marug4580@gmail.com>
2019-10-12 08:55:17 +0000

    :ocean: docker pull and portbinding

--- api/d3.go
+++ api/d3.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 
 	"github.com/ak1ra24/microns/utils"
+	"github.com/docker/docker/client"
+	"golang.org/x/net/context"
 )
 
 var (
@@ -121,4 +123,22 @@ func Confirm(s string) bool {
 	}
 
 	return false
+}
+
+// CreateFrontend func is Image: akiranet24/microns-frontend docker container start
+func CreateFrontend() error {
+
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		return err
+	}
+
+	c := NewContainer(ctx, cli)
+
+	if err := c.CreateContainerPort("akiranet24/microns-frontend", "microns-frontend", "80", "8080"); err != nil {
+		return err
+	}
+
+	return nil
 }
